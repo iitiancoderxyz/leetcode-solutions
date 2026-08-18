e4f85bf7d733c7b99c755d781dcb639fad5619fd
Pavan Patil <iitianpavangamer327@gmail.com>
2026-08-19 01:44:21 +0530

    Time: 11 ms (16.08%), Space: 21.6 MB (18.64%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,8 +1,12 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        from collections import Counter
-        freq=Counter(nums)
-        for key in freq:
-            if freq[key]==1:
-                return key
+        d={}
+        for i in range (len(nums)):
+            if nums[i] not in d:
+                d.update({nums[i]:1})
+            else:
+                d[nums[i]]+=1
+        for j in d:
+            if d[j]==1:
+                return j
                 break
