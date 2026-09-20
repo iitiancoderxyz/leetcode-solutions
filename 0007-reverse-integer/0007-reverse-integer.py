286c85104fb1c8a109ec3f3f6b298a0a1fb42898
Pavan Patil <iitianpavangamer327@gmail.com>
2026-09-20 16:11:44 +0530

    Time: 0 ms (100%), Space: 19.3 MB (33.78%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -6,7 +6,5 @@ class Solution:
             elif x<0:
                 a=int(str(-1*x)[::-1])*-1
                 return a if a>-2**31 else 0
-            else:
-                return 0
 
             
